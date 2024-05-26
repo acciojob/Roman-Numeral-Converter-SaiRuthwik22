f3d273c427a2a06fb8e7f78254d74ebe7c220927
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-05-26 18:32:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,25 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let result =""
+	for (let i = 0; i <=6; i++) {
+		while(num>=obj[i][1]){
+			num -= obj[i][1]
+			result+=obj[i][0]
+		}
+	}
+	result = result.replace('DCCCC', 'CM');
+    result = result.replace('CCCC', 'CD');
+    result = result.replace('LXXXX', 'XC');
+    result = result.replace('XXXX', 'XL');
+    result = result.replace('VIIII', 'IX');
+    result = result.replace('IIII', 'IV');
+	
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
